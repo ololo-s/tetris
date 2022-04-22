79c23c5d782a64a11e970e97b27b5fa3f3f56288
Olha Solovei <o.keksovna@gmail.com>
2022-04-22 12:43:14 +0300

    handle user keys correctly and refresh board

--- src/Tetris.svelte
+++ src/Tetris.svelte
@@ -2,18 +2,35 @@
   import Board from "./Board.svelte";
 
   let board = Array(20).fill([]).map(() => Array(10).fill(' '))
-  let dot = {y: 0, x: 5}
+  let dot = {y: 0, x: 4}
+  let oldDot = {...dot}
 
+  function handleKey(e: KeyboardEvent) {
+      switch (e.key) {
+          case 'ArrowLeft': return moveLeft()
+          case 'ArrowRight': return moveRight()
+      }
+  }
 
+  function moveLeft () {
+      dot.x--
+  }
 
-  // setInterval(() => {
-  //     board[dot.y][dot.x] = ' '
-  //     dot.y++
-  //     board[dot.y][dot.x] = '.'
-  // }, 1000)
+  function moveRight () {
+      dot.x++
+  }
+
+  setInterval(() => {
+      dot.y++
+  }, 1000)
+
+  $: {
+      board[oldDot.y][oldDot.x] = ' '
+      board[dot.y][dot.x] = '.'
+      oldDot = {...dot}
+  }
 </script>
 
-<svelte:window on:keydown={e => console.log(e.key)}/>
+<svelte:window on:keydown={handleKey}/>
 
 <Board {board}/>
-
