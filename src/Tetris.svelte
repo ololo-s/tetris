43ea24e2ec696794b725fb890fb38adcc0a0ae79
Olha Solovei <o.keksovna@gmail.com>
2022-04-22 16:37:17 +0300

    handle dots hitting other dots

--- src/Tetris.svelte
+++ src/Tetris.svelte
@@ -1,51 +1,58 @@
 <script lang="ts">
-  import Board from "./Board.svelte";
+    import Board from "./Board.svelte";
 
-  const height = 20
-  const width = 10
-  let board = Array(height).fill([]).map(() => Array(width).fill(' '))
+    const height = 20
+    const width = 10
+    let board = Array(height).fill([]).map(() => Array(width).fill(' '))
 
-  const newDot = {y: 0, x: 4}
-  let dot = {...newDot}
-  let oldDot = {...dot}
+    const newDot = {y: 0, x: 4}
+    let dot = {...newDot}
+    let oldDot = {...dot}
 
-  function handleKey(e: KeyboardEvent) {
-      switch (e.key) {
-          case 'ArrowLeft': return moveLeft()
-          case 'ArrowRight': return moveRight()
-          case 'ArrowDown': return fastDown()
-      }
-  }
+    function handleKey(e: KeyboardEvent) {
+        switch (e.key) {
+            case 'ArrowLeft':
+                return moveLeft()
+            case 'ArrowRight':
+                return moveRight()
+            case 'ArrowDown':
+                return fastDown()
+        }
+    }
 
-  function moveLeft() {
-      dot.x--
-      if (dot.x < 0) dot.x = 0
-  }
+    function moveLeft() {
+        dot.x--
+        if (dot.x < 0) dot.x = 0
+    }
 
-  function moveRight() {
-      dot.x++
-      if (dot.x >= width) dot.x--
-  }
+    function moveRight() {
+        dot.x++
+        if (dot.x >= width) dot.x--
+    }
 
-  function fastDown() {
-      const timer = setInterval(() => {
-          dot.y++
-          if (dot.y >= height) clearInterval(timer)
-      }, 50)
-  }
+    let fastTimer: any
+    function fastDown() {
+        fastTimer = setInterval(() => dot.y++, 50)
+    }
 
-  setInterval(() => dot.y++, 1000)
+    setInterval(() => dot.y++, 1000)
 
-  $: if (dot.y >= height) {
-      dot.y--
-      oldDot = dot = {...newDot}
-  }
+    $: handleDotFinalPosition(dot)
+    $: drawDot(dot)
 
-  $: {
-      board[oldDot.y][oldDot.x] = ' '
-      board[dot.y][dot.x] = '.'
-      oldDot = {...dot}
-  }
+    function handleDotFinalPosition(d: typeof d) {
+        if (d.y >= height || board[d.y][d.x] != ' ') {
+            clearInterval(fastTimer)
+            d.y--
+            dot = oldDot = {...newDot}
+        }
+    }
+
+    function drawDot(dot: typeof dot) {
+        board[oldDot.y][oldDot.x] = ' '
+        board[dot.y][dot.x] = '.'
+        oldDot = {...dot}
+    }
 </script>
 
 <svelte:window on:keydown={handleKey}/>
