be3df38eeaeb6614ce22f47998f3059a53050978
Olha Solovei <o.keksovna@gmail.com>
2022-04-28 12:04:05 +0300

    issue random figures each time

---
+++ src/Figure.ts
@@ -0,0 +1,10 @@
+export class Dot {
+    constructor(
+        public x: number,
+        public y: number,
+    ) {}
+}
+
+type Figure = Dot[]
+
+export default Figure
--- src/Tetris.svelte
+++ src/Tetris.svelte
@@ -1,23 +1,41 @@
 <script lang="ts">
     import Board from "./Board.svelte";
+    import Figure, {Dot} from "./Figure";
 
-    interface Dot {
-        x: number,
-        y: number,
-    }
+    let figures: Figure[] = [
+        [{x: 4, y: 0}, {x: 5, y: 0}, {x: 4, y: 1}, {x: 5, y: 1}], //квадрат
 
-    type Figure = Dot[]
+        [{x: 3, y: 0}, {x: 4, y: 0}, {x: 5, y: 0}, {x: 6, y: 0}], // палка        // [{x: 4, y: 0}, {x: 4, y: 1}, {x: 4, y: 2}, {x: 4, y: 3}], палка перевер
 
-    const figures: Figure[] = [
-        [{y: 0, x: 4}],
-        [{x: 4, y: 0}, {x: 5, y: 0}, {x: 4, y: 1}, {x: 5, y: 1}]
+        // [{x: 5, y: 0}, {x: 4, y: 1}, {x: 5, y: 1}, {x: 6, y: 1}], //торт
+        // [{x: 5, y: 0}, {x: 4, y: 1}, {x: 5, y: 1}, {x: 5, y: 2}], //торт 1влево
+        // [{x: 4, y: 0}, {x: 5, y: 0}, {x: 6, y: 0}, {x: 5, y: 1}], //торт 2 влево/2вправо
+        // [{x: 5, y: 0}, {x: 5, y: 1}, {x: 6, y: 1}, {x: 5, y: 2}], //торт 3 влево/1 вправо
+
+
+        // [{x: 4, y: 0}, {x: 4, y: 1}, {x: 4, y: 2}, {x: 5, y: 2}], // L
+        // [{x: 6, y: 0}, {x: 4, y: 1}, {x: 5, y: 1}, {x: 6, y: 1}], // L 1 влево
+        // [{x: 4, y: 0}, {x: 5, y: 0}, {x: 5, y: 1}, {x: 5, y: 2}], // L 2 влево/ 2 вправо
+        // [{x: 4, y: 0}, {x: 5, y: 0}, {x: 6, y: 0}, {x: 6, y: 1}], // L 3 влево/ 1 вправо
+
+
+        // [{x: 5, y: 0}, {x: 6, y: 0}, {x: 4, y: 1}, {x: 5, y: 1}] //z
+        // [{x: 4, y: 0}, {x: 4, y: 1}, {x: 5, y: 1}, {x: 5, y: 2}] //z 1 влево/1 вправо
+
     ]
 
+
+    let figureId = 0
     const height = 20
     const width = 10
     let board = Array(height).fill([]).map(() => Array(width).fill(' '))
     let figure: Figure, oldFigure: Figure
     issueNewFigure()
+
+    function randomFigure() {
+        figureId = Math.floor(Math.random() * figures.length)
+        return figureId
+    }
 
     function deepCopy(figure: Figure): Figure {
         return figure.map(d => ({...d}))
